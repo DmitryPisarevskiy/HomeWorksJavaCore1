87666b0763ada5c55c1f450c196787eaa32f0248
Dmitry <f_o_r_w_a_r_d@inbox.ru>
2020-05-28 22:21:12 +0500

    hw5

--- src/lesson5/College.java
+++ src/lesson5/College.java
@@ -5,5 +5,91 @@ import com.sun.org.apache.xpath.internal.objects.XString;
 import java.util.Scanner;
 
 public class College {
+    private String fio;
+    private String position;
+    private String eMail;
+    private long phone;
+    private int salary;
+    private byte age;
+    private static Scanner sc=new Scanner(System.in);
 
+
+
+    public College(String fio, String position, String eMail, long phone, int salary, byte age) {
+        this.fio = fio;
+        this.position = position;
+        this.seteMail(eMail);
+        this.setPhone(phone);
+        this.salary = salary;
+        this.setAge(age);
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void printCollege() {
+        System.out.println("ФИО - " + fio);
+        System.out.println("Должность - " + position);
+        System.out.println("email - " + eMail);
+        System.out.println("Телефон - " + phone);
+        System.out.println("Зарплата, рублей - " + salary);
+        System.out.println("Возраст, лет - " + age + "\n");
+    }
+
+    public void seteMail(String eMail) {
+        do {
+            if (!eMail.contains("@")) {
+                System.out.println("Введите электронный адрес, содержащий символ @, для сотрудника " + this.fio);
+                eMail=sc.nextLine();
+            } else {
+                break;
+            }
+        } while (true);
+        this.eMail = eMail;
+    }
+
+    public void setAge(byte age) {
+        do {
+            if (age<1) {
+                System.out.println("Введите корректный возраст для сотрудника " + this.fio);
+                age=sc.nextByte();
+            } else {
+                break;
+            }
+        } while (true);
+        this.age = age;
+    }
+
+    public void setPhone(long phone) {
+        do {
+            if (phone>100000000000L || phone<10000000000L) {
+                System.out.println("Введите номер телефона из 11 цифрф без знака + для сотрудника " + this.fio);
+                phone=sc.nextLong();
+            } else {
+                break;
+            }
+        } while (true);
+        this.phone = phone;
+    }
 }
--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -2,6 +2,16 @@ package lesson5;
 
 public class Main {
     public static void main(String[] args) {
-
+        College[] colleges = new College[5];
+        colleges[0] = new College("Петров Петр Петрович", "главный специалист", "petro@xmail.ru", 89756541541L, 45231, (byte)(55));
+        colleges[1] = new College("Иванов Иван Иванович", "начальник отдела", "ivan@xmail.ru", 83456255999L, 100000, (byte)(46));
+        colleges[2] = new College("Сидоров Сидор Сидорович", "ведущий специалист", "sidor@xmail.ru", 81239875495L, 35231, (byte)(23));
+        colleges[3] = new College("Анжеликова Анжела Варумовна", "младший специалист", "angi@xmail.ru", 83476595123L, 25231, (byte)(23));
+        colleges[4] = new College("Пугачев Максим Киркорович", "заместитель начальника отдела", "max@xmail.ru", 87896548789L, 75231, (byte)(42));
+        for (int i = 0; i < colleges.length; i++) {
+            if (colleges[i].getAge()>40){
+                colleges[i].printCollege();
+            }
+        }
     }
 }
